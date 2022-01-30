616e41345c148812ca102423896819182fe78160
VimFreeman <github@freiman.cc>
2022-01-29 22:49:55 -0500

    Create receive method

--- include/Twilio.hpp
+++ include/Twilio.hpp
@@ -1,6 +1,16 @@
 #pragma once
 #include <string>
 
+
+
 namespace Twilio {
+
+    struct MemoryStruct {
+    char *memory;
+    size_t size;
+    };
+
     void send_message(std::string& message_body) {}
+
+    MemoryStruct get_messages() {}
 }
